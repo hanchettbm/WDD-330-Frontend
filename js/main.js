de49a69d57c114301a921a2cd40b501edb9f18d6
Baden <hanchettbm@gmail.com>
2022-01-08 19:05:18 -0700

    Week 1

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const links = [
+    {
+      label: "Week 1",
+      url: "week1/index.html"
+    },
+    {
+        label: "Week 2",
+        url: "week2/index.html"
+      }
+  ]
+
+links.forEach((link) => { 
+    let newListItem = document.createElement("li");
+    let newLink = document.createElement("a");
+    let newLinkText = document.createTextNode(link.label);
+    newLink.appendChild(newLinkText);
+    newLink.href = link.url;
+    console.log(newLink);
+    newListItem.appendChild(newLink);
+    let orderedList = document.querySelector(".ordered-list");
+    orderedList.appendChild(newListItem);
+})
