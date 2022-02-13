45476fc55369a92e24132a4e5cbcd6efbc7fde8a
Baden <hanchettbm@gmail.com>
2022-02-13 00:19:32 -0700

    Todo

--- week6/todo/js/todo.js
+++ week6/todo/js/todo.js
@@ -24,17 +24,29 @@ button.addEventListener('click', function(){
     listButton.onclick = function(e) {
         list.removeChild(listItem);
     }
+    
 
     input.focus();
 
+    var li = document.querySelectorAll('li');
+    for(var i = 0 ; i < li.length; i++){
+        li[i].addEventListener('click', function(event) {
+        var input = this.querySelector('input[type=checkbox]');
+        if (input.checked){
+            function strikeThrough(text) {
+                return text
+                  .split('')
+                  .map(char => char + '\u0336')
+                  .join('')
+              }
+              listText.innerText = strikeThrough(listText.innerText);
+        }
+
+        if (input.unchecked){
+            listText.innerText = listText.innerText.replace(/[\u0336]/g, '');
+        }
+        });
+    }
+
 });
 
-if ( document.getElementById("checkbox").checked == true) {
-    console.log("checked");
- function strike(text) {
-     var result = "<del>" + text + "</del>";
-     return result;
-   }
-
-   listText.innerHTML = strike(listText.textContent);
-}
