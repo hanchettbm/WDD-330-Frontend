cbe5dc9ac15f3c016084951bfdcbdcfa6723529c
Baden <hanchettbm@gmail.com>
2022-03-19 23:25:51 -0600

    Week 11

--- js/main.js
+++ js/main.js
@@ -39,6 +39,10 @@ const links = [
         label: "Week 10",
         url: "week10/index.html"
       },
+      {
+        label: "Week 11",
+        url: "week11/index.html"
+      },
   ]
 
 links.forEach((link) => { 
